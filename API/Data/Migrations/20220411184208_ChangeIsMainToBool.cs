fc57748af47ae28fce36b631c197e7f45e0a2342
Krzysztof Hermaszewski <hermaszewski.krzysztof@gmail.com>
2022-04-11 21:12:01 +0200

    Added AutoMapper

---
+++ API/Data/Migrations/20220411184208_ChangeIsMainToBool.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace API.Migrations
+{
+    public partial class ChangeIsMainToBool : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<bool>(
+                name: "IsMain",
+                table: "Photos",
+                type: "bit",
+                nullable: false,
+                defaultValue: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "IsMain",
+                table: "Photos",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(bool),
+                oldType: "bit");
+        }
+    }
+}
