10d67f2d9e0dbe5f855a7a8d0f9025faac29d9c1
Krzysztof Hermaszewski <hermaszewski.krzysztof@gmail.com>
2022-04-21 19:16:32 +0200

    Added navbar

---
+++ API/Data/Migrations/20220412163751_interestToInterests.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace API.Migrations
+{
+    public partial class interestToInterests : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Interest",
+                table: "Users",
+                newName: "Interests");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Interests",
+                table: "Users",
+                newName: "Interest");
+        }
+    }
+}
