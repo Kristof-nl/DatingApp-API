2e5347b8ef05e8fb258f807caf3138230bb347bd
Krzysztof Hermaszewski <hermaszewski.krzysztof@gmail.com>
2022-05-28 18:22:41 +0200

    Start with pagination

---
+++ API/Helpers/PaginationHeader.cs
@@ -0,0 +1,18 @@
+﻿namespace API.Helpers
+{
+    public class PaginationHeader
+    {
+        public PaginationHeader(int CurrentPage, int ItemsPerPage, int TotalItems, int TotalPages)
+        {
+            CurrentPage = CurrentPage;  
+            ItemsPerPage = ItemsPerPage;
+            TotalItems = TotalItems;
+            TotalPages = TotalPages;
+        }
+
+        public int CurrentPage { get; set; }
+        public int ItemsPerPage { get; set; }
+        public int TotalItems { get; set; }
+        public int TotalPages { get; set; }
+    }
+}
